12a58d38462a4f98bcd2df05d2668dbd1e57e68c
yuki-konn <gakusei.saru@gmail.com>
2025-03-20 05:46:19 -0500

    added babel preset for TypeScript

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+export default {
+  // @babel/preset-react PROVIDES INSTRUCTIONS ON HOW TO CONVERT HTML TO JAVASCRIPT
+  // @babel/preset-env IS FOR JEST TO WORK,
+  // @babel/preset-typescript CONVERTS TS TO JS
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-react",
+    "@babel/preset-typescript",
+  ],
+};
