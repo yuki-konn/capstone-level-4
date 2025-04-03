69da4e882d28bdae20cf7a6f694516e3bc35a55c
yuki-konn <gakusei.saru@gmail.com>
2025-04-03 04:03:11 -0500

    created tests for api response functions

---
+++ backend/src/modules/getQuote.test.ts
@@ -0,0 +1,49 @@
+import { getQuote } from "./getQuote";
+
+describe("getQuote function (API response)", () => {
+  // Steps 1 to 3
+  test("returns a response", async () => {
+    // ARRANGE
+    let response: {
+      author: string;
+      quote: string;
+      tags: Array<string>;
+      url: string;
+    };
+    // ACT
+    response = await getQuote();
+    // ASSERT
+    expect(response).toHaveProperty("author");
+    expect(response).toHaveProperty("quote");
+    expect(response).toHaveProperty("tags");
+    expect(response).toHaveProperty("url");
+  });
+  test("return is not the same quote or url", async () => {
+    // ARRANGE
+    let response1: {
+      author: string;
+      quote: string;
+      tags: Array<string>;
+      url: string;
+    };
+    let response2: {
+      author: string;
+      quote: string;
+      tags: Array<string>;
+      url: string;
+    };
+    // ACT
+    response1 = await getQuote();
+    response2 = await getQuote();
+
+    // ASSERT
+    expect(response1).toHaveProperty("quote");
+    expect(response1).toHaveProperty("url");
+
+    expect(response2).toHaveProperty("quote");
+    expect(response2).toHaveProperty("url");
+
+    expect(response1.quote).not.toBe(response2.quote);
+    expect(response1.url).not.toBe(response2.url);
+  });
+});
---
+++ backend/src/modules/getTrivia.test.ts
@@ -0,0 +1,38 @@
+import { getTrivia } from "./getTrivia";
+describe("getTrivia function (API response)", () => {
+  test("accepts a query object with amount, category, difficulty and type", async () => {
+    // ARRANGE
+    const query = {
+      amount: 1,
+      category: 9,
+      difficulty: "medium",
+      type: "multiple",
+    };
+    // ACT
+    const result = await getTrivia(query);
+    // ASSERT
+    expect(result).toBeDefined;
+  });
+  test("accepts a query object and returns a response", async () => {
+    // ARRANGE
+    const query = {
+      amount: 1,
+      category: 9,
+      difficulty: "medium",
+      type: "multiple",
+    };
+    // ACT
+    const result = await getTrivia(query);
+    const question: string = result.question;
+    const choices: Array<string> = result.choices;
+    const answer: string = result.answer;
+    // ASSERT
+    expect(question).toBeTruthy();
+    expect(choices).toBeInstanceOf(Object);
+    expect(answer).toBeNaN;
+  });
+});
+
+// tests work individually but don't work when together
+// my guess is that the requests are too often and getting blocked by the api
+// https://opentdb.com/api.php?amount=1&category=9&difficulty=medium&type=multiple
