75bd7e8fef3950d5e6b46c850a079217760d6949
yuki-konn <gakusei.saru@gmail.com>
2025-04-07 18:37:57 -0500

    created Quote module

---
+++ frontend/src/Views/Quote.tsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import React, { useState } from "react";
+import "./Quote.scss";
+import { Link } from "react-router";
+
+export function Quote() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [url, setUrl] = useState("");
+  const [tags, setTags] = useState("");
+
+  return (
+    <div id="quote-module">
+      <h3>Quotes to think about while drinking tea.</h3>
+      <button className="btn btn-success btn-sm" onClick={handleClick}>
+        Brew Quote
+      </button>
+      <div id="quote-display">
+        <blockquote id="quote">{quote}</blockquote>
+        <blockquote id="author">{author}</blockquote>
+        <p id="url">
+          <Link to={url} target="_blank">
+            {url}
+          </Link>
+        </p>
+        <p id="tags">{tags}</p>
+      </div>
+    </div>
+  );
+
+  async function handleClick() {
+    const response = await axios.get("http://localhost:8000/quote");
+    const quote = response.data.quote;
+    const author = response.data.author;
+    const url = response.data.url;
+    const tags = response.data.tags;
+    setQuote(`"${quote}"`);
+    setAuthor(`~ ${author}`);
+    setUrl("Link: " + `${url}`);
+    setTags("Quote Tags: " + `${tags}`);
+  }
+}
