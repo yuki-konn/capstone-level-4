1736fb1bc91c06bdcc1b872cea1dd7bb96f88748
yuki-konn <gakusei.saru@gmail.com>
2025-04-14 19:08:42 -0500

    updated typescript declaration file

--- frontend/index.d.ts
+++ frontend/index.d.ts
@@ -6,5 +6,5 @@ declare module "*.jpg";
 // declare module "*.mp4";
 
 // Add functions and objects from external scripts here:
-declare const bootstrap: any; //Object
-declare const imageMapResize: any; //Function
+declare const bootstrap: { Tooltip: (element: HTMLElement) => void }; //Object
+declare const imageMapResize: () => void; //Function
