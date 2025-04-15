de40ccd11e2c828614fdba31814cabf54cc58abe
yuki-konn <gakusei.saru@gmail.com>
2025-04-15 17:42:49 -0500

    added comment

--- backend/src/routes/authUser.ts
+++ backend/src/routes/authUser.ts
@@ -7,10 +7,10 @@ export async function authUser(request: Request, response: Response) {
   const password = request.query.password;
 
   // USED TEMPLATE STRINGS TO CONVERT TO STRING.
+  // BECAUSE I DON'T WANT THE QUOTATION MARKS FROM JSON.STRINGIFY
   const isAuthenticated: boolean = await authenticateUser(
     `${email}`,
     `${password}`
   );
-  debugger;
   response.send(isAuthenticated);
 }
