a4784f5892b10efc5bf6b54d2b3dc67cd9d191ad
yuki-konn <gakusei.saru@gmail.com>
2025-04-15 11:41:44 -0500

    added webpack library files

--- backend/webpack.config.ts
+++ backend/webpack.config.ts
@@ -15,5 +15,10 @@ export default {
     extensions: [".js", ".ts"], // Automatically inserts file extensions in import statements
   },
   target: "node", // Bundles for nodeJS
-  output: { filename: "index.js" }, // Outputs a bundle file with the file name as index.js (default is main.js)
+  output: {
+    filename: "index.js", // Outputs a bundle file with the file name as index.js (default is main.js)
+    library: {
+      type: "commonjs", // Prevents tree shaking of unused bundle exports. Indicates that the bundle is a library.
+    },
+  },
 } as Configuration;
