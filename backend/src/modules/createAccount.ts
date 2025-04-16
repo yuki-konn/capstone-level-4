8d6ae91814974b241397da59c67bad7ad41feef3
yuki-konn <gakusei.saru@gmail.com>
2025-04-15 23:00:17 -0500

    added createAccount function

--- backend/src/modules/createAccount.ts
+++ backend/src/modules/createAccount.ts
@@ -2,11 +2,46 @@ import { PutCommandInput } from "@aws-sdk/lib-dynamodb";
 import { Account } from "./Account";
 import { dynamoDBClient } from "./dynamoDBclient";
 
-export async function createAccount(newAccount: Account) {
-  const request: PutCommandInput = {
-    TableName: "capstone_logins",
-    Item: newAccount,
-  };
-  const response = await dynamoDBClient().put(request);
-  return response;
+export async function createAccount(newAccount: Account): Promise<any> {
+  const isUndefined = newAccount === undefined;
+  if (isUndefined)
+    return console.error(
+      "Error: newAccount is undefined. Unable to create account."
+    );
+  const { email, password, userName, firstName, lastName, phone } = newAccount;
+  const isAllEmpty =
+    email === "" &&
+    password === "" &&
+    userName === "" &&
+    firstName === "" &&
+    lastName === "" &&
+    phone === "";
+  const isEmailOrPasswordEmpty = email === "" || password === "";
+  const hasRequiredInputs = email !== "" && password !== "" && userName !== "";
+  const isNotObject = JSON.stringify(newAccount) === "{}";
+
+  if (isAllEmpty)
+    return console.error(
+      "Error: All input fields are empty. Unable to create account."
+    );
+
+  if (isEmailOrPasswordEmpty)
+    return console.error(
+      "Error: The email or password field is empty. Unable to create account."
+    );
+
+  if (isNotObject)
+    return console.error(
+      "Error: newAccount object has no properties. Unable to create account."
+    );
+
+  if (hasRequiredInputs) {
+    const request: PutCommandInput = {
+      TableName: "capstone_logins",
+      Item: newAccount,
+    };
+    const response = await dynamoDBClient().put(request);
+    return response;
+  }
+  console.log("Error: Unable to create account");
 }
