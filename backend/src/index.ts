cb9dfa91487fc6a27098ef3357f66444e47ae688
yuki-konn <gakusei.saru@gmail.com>
2025-04-16 18:24:26 -0500

    added read path for express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import { quote } from "./routes/quote";
 import { authUser } from "./routes/authUser";
 import serverless from "serverless-http";
 import { create } from "./routes/create";
+import { read } from "./routes/read";
 
 const hostname = "localhost"; // Local domain
 const port = 8000; // Common backend ports 8000, 9000, 3000
@@ -19,6 +20,7 @@ express.get(path, root);
 express.get("/quote", quote); // FavQ quote API path
 express.get("/authUser", authUser); // User Authentication path
 express.get("/create", create); // Account Creation path
+express.get("/read", read); // Read Account path
 // listen method runs the handler.
 const isRunningLocally = process.env.mode === "development";
 if (isRunningLocally) express.listen(port, hostname, handleListen);
