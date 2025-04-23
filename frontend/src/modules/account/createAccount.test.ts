b0cc294a10a3788b70baa5f3bf053ee30b061351
yuki-konn <gakusei.saru@gmail.com>
2025-04-22 20:13:59 -0500

    created createAccount test

---
+++ frontend/src/modules/account/createAccount.test.ts
@@ -0,0 +1,27 @@
+describe("createAccount function", () => {
+  it("", () => {
+    // ARRANGE
+    // ACT
+    // ASSERT
+  });
+  it.skip("", () => {
+    // ARRANGE
+    // ACT
+    // ASSERT
+  });
+  it.skip("", () => {
+    // ARRANGE
+    // ACT
+    // ASSERT
+  });
+  it.skip("", () => {
+    // ARRANGE
+    // ACT
+    // ASSERT
+  });
+  it.skip("", () => {
+    // ARRANGE
+    // ACT
+    // ASSERT
+  });
+});
