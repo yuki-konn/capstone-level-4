c1dfaa8e87a8d5e7438819a6ce20ce93906fb784
yuki-konn <gakusei.saru@gmail.com>
2025-04-23 19:18:36 -0500

    updated function according to test specifications

--- frontend/src/modules/account/createAccount.ts
+++ frontend/src/modules/account/createAccount.ts
@@ -1,1 +1,17 @@
-export function createAccount() {}
+import axios from "axios";
+import { Account } from "./Account";
+
+export async function createAccount(targetAccount: Account) {
+  const isQueryMissing = JSON.stringify(targetAccount) === "{}";
+  if (isQueryMissing)
+    return "Error: newAccount object has no properties. Unable to create account.";
+  const { email, password, userName, firstName, lastName, phone } =
+    targetAccount;
+  const query = `?email=${email}&password=${password}&userName=${userName}&firstName=${firstName}&lastName=${lastName}&phone=${phone}`;
+  const rootpath = "http://localhost:8000";
+  const route = "/create";
+  const url = rootpath + route + query;
+  const response = await axios.get(url);
+  const message = response.data;
+  return message;
+}
