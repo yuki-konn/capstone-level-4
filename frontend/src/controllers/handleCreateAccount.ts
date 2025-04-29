628f9569e0430571c964499725e0cfe3a6a924d7
yuki-konn <gakusei.saru@gmail.com>
2025-04-28 19:53:25 -0500

    added handler for modal form submission to create account

---
+++ frontend/src/controllers/handleCreateAccount.ts
@@ -0,0 +1,25 @@
+import { Account } from "../modules/account/Account";
+import { createAccount } from "../modules/account/createAccount";
+
+export async function handleCreateAccount(event: any) {
+  event.preventDefault();
+
+  const targetEmail = event.target[1].value;
+  const targetPassword = event.target[2].value;
+  const targetUserName = event.target[3].value;
+  const targetFirstName = event.target[4].value;
+  const targetLastName = event.target[5].value;
+  const targetPhone = event.target[6].value;
+
+  const newAccount: Account = {
+    email: targetEmail,
+    password: targetPassword,
+    userName: targetUserName,
+    firstName: targetFirstName,
+    lastName: targetLastName,
+    phone: targetPhone,
+  };
+
+  const response = await createAccount(newAccount);
+  return response;
+}
