df2d42731cdaa85db3b18be880781ab6e3b91262
yuki-konn <gakusei.saru@gmail.com>
2025-04-29 18:57:38 -0500

    created test draft for readAccount

---
+++ frontend/src/modules/account/readAccount.test.ts
@@ -0,0 +1,7 @@
+describe("readAccount function", () => {
+  it("returns email, password, userName, firstName, lastName and phone when matching email and password is provided.", () => {});
+  it("returns an error message when email is not in the list", () => {});
+  it("returns an error message when email is in the list but password doesn't match", () => {});
+  it("returns an error message when email or password is an object", () => {});
+  it("returns an error message when email or password is undefined", () => {});
+});
