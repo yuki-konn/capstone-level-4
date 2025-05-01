98994db1c6f8d47284e8747316cacaa2919b9af4
yuki-konn <gakusei.saru@gmail.com>
2025-05-01 03:20:24 -0500

    created tests for updateAccount function

---
+++ frontend/src/modules/account/updateAccount.test.ts
@@ -0,0 +1,77 @@
+import { Account } from "./Account";
+import { updateAccount } from "./updateAccount";
+
+describe("updateAccount function", () => {
+  it("returns success message if account is updated.", async () => {
+    // ARRANGE
+    const account: Account = {
+      email: "test3@email.com",
+      password: "test3",
+      userName: "testUser3",
+      firstName: "Test3",
+      lastName: "Testing",
+      phone: 1112223333,
+    };
+    // ACT
+    const result = await updateAccount(account);
+    // ASSERT
+    expect(result).toBe("Your account has been updated.");
+  });
+  it("returns error message if email (partition key) is empty", async () => {
+    // ARRANGE
+    const account: Account = {
+      email: "",
+      password: "test3",
+      userName: "testUser3",
+      firstName: "Test3",
+      lastName: "Testing",
+      phone: 1112223333,
+    };
+    // ACT
+    const result = await updateAccount(account);
+    // ASSERT
+    expect(result).toBe(
+      "Your account wasn't able to update because your email is undefined."
+    );
+  });
+  it("returns error message when the email (partition key) is in the list but new attribute values are not in the list.", async () => {
+    // ARRANGE
+    const account: any = {
+      email: "test3@email.com",
+      forSale: true,
+    };
+    // ACT
+    const result = await updateAccount(account);
+    // ASSERT
+    expect(result).toBe(
+      "Your account wasn't able to update because the changes made are invalid."
+    );
+  });
+  it("returns false when the email (partition key) is an object", async () => {
+    // ARRANGE
+    const account: Account = {
+      email: {} as any,
+      password: "test3",
+      userName: "testUser3",
+      firstName: "Test3",
+      lastName: "Testing",
+      phone: 1112223333,
+    };
+    // ACT
+    const result = await updateAccount(account);
+    // ASSERT
+    expect(result).toBe(
+      "Your account wasn't able to update because your email is invalid."
+    );
+  });
+  it("returns false when account is undefined", async () => {
+    // ARRANGE
+    const account: Account = undefined;
+    // ACT
+    const result = await updateAccount(account);
+    // ASSERT
+    expect(result).toBe(
+      "Your account wasn't able to update because your account is undefined."
+    );
+  });
+});
