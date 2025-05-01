2c97c7979d2b6af630d51ab7cfb50032925f7a2b
yuki-konn <gakusei.saru@gmail.com>
2025-05-01 07:20:59 -0500

    updated 'remove' route handler

--- backend/src/routes/remove.ts
+++ backend/src/routes/remove.ts
@@ -4,10 +4,6 @@ import { Account } from "../modules/Account";
 
 export async function remove(request: Request, response: Response) {
   const query = request.query;
-  const deletedAccount = await deleteAccount(query as Account);
-  const isSuccessful = deletedAccount === true;
-
-  if (isSuccessful) response.send("Your account has been deleted.");
-  if (!isSuccessful)
-    response.send("Your account was unable to be deleted. Please try again.");
+  const removeAccount = await deleteAccount(query as Account);
+  response.send(removeAccount);
 }
