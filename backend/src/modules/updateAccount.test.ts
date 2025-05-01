23af50c68c32f46ba986163778be5612b302227d
yuki-konn <gakusei.saru@gmail.com>
2025-04-30 22:38:00 -0500

    updated test return type for backend updateAccount function

--- backend/src/modules/updateAccount.test.ts
+++ backend/src/modules/updateAccount.test.ts
@@ -2,7 +2,7 @@ import { Account } from "./Account";
 import { updateAccount } from "./updateAccount";
 
 describe("updateAccount function", () => {
-  it("returns httpStatusCode 200 if account is updated.", async () => {
+  it("returns success message if account is updated.", async () => {
     // ARRANGE
     const account: Account = {
       email: "test3@email.com",
@@ -13,11 +13,11 @@ describe("updateAccount function", () => {
       phone: 1112223333,
     };
     // ACT
-    const result = await updateAccount(account);
+    const result: string = await updateAccount(account);
     // ASSERT
-    expect(result).toHaveProperty("$metadata.httpStatusCode", 200);
+    expect(result).toBe("Your account has been updated.");
   });
-  it("returns false if email (partition key) is empty", async () => {
+  it("returns error message if email (partition key) is empty", async () => {
     // ARRANGE
     const account: Account = {
       email: "",
@@ -28,20 +28,24 @@ describe("updateAccount function", () => {
       phone: 1112223333,
     };
     // ACT
-    const result = await updateAccount(account);
+    const result: string = await updateAccount(account);
     // ASSERT
-    expect(result).toBe(false);
+    expect(result).toBe(
+      "Your account wasn't able to update because your email is undefined."
+    );
   });
-  it("returns false when the email (partition key) is in the list but new attribute values are not in the list.", async () => {
+  it("returns error message when the email (partition key) is in the list but new attribute values are not in the list.", async () => {
     // ARRANGE
     const account: any = {
       email: "test3@email.com",
       forSale: true,
     };
     // ACT
-    const result = await updateAccount(account);
+    const result: string = await updateAccount(account);
     // ASSERT
-    expect(result).toBe(false);
+    expect(result).toBe(
+      "Your account wasn't able to update because the changes made are invalid."
+    );
   });
   it("returns false when the email (partition key) is an object", async () => {
     // ARRANGE
@@ -54,16 +58,20 @@ describe("updateAccount function", () => {
       phone: 1112223333,
     };
     // ACT
-    const result = await updateAccount(account);
+    const result: string = await updateAccount(account);
     // ASSERT
-    expect(result).toBe(false);
+    expect(result).toBe(
+      "Your account wasn't able to update because your email is invalid."
+    );
   });
   it("returns false when account is undefined", async () => {
     // ARRANGE
     const account: Account = undefined;
     // ACT
-    const result = await updateAccount(account);
+    const result: string = await updateAccount(account);
     // ASSERT
-    expect(result).toBe(false);
+    expect(result).toBe(
+      "Your account wasn't able to update because your account is undefined."
+    );
   });
 });
