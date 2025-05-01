11ef26ff7afbd3077d85257205ca0d8b3f9bdd44
yuki-konn <gakusei.saru@gmail.com>
2025-05-01 07:57:04 -0500

    created deleteAccount form submission handler

---
+++ frontend/src/controllers/handleDeleteAccount.ts
@@ -0,0 +1,21 @@
+import { Account } from "../modules/account/Account";
+import { deleteAccount } from "../modules/account/deleteAccount";
+
+export async function handleDeleteAccount(event: any): Promise<string> {
+  event.preventDefault();
+
+  const targetEmail = event.target[0].value;
+  const targetPassword = event.target[1].value;
+
+  const newAccount: Account = {
+    email: targetEmail,
+    password: targetPassword,
+    userName: "",
+    firstName: "",
+    lastName: "",
+    phone: "",
+  };
+
+  const response = await deleteAccount(newAccount);
+  return response;
+}
