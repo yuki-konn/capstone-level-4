01dee8016c09e047dedd81d50ae806fccf0ca1bf
yuki-konn <gakusei.saru@gmail.com>
2025-05-01 05:25:43 -0500

    updated tests for updateAccount

--- backend/src/modules/updateAccount.test.ts
+++ backend/src/modules/updateAccount.test.ts
@@ -34,17 +34,21 @@ describe("updateAccount function", () => {
       "Your account wasn't able to update because your email is undefined."
     );
   });
-  it("returns error message when the email (partition key) is in the list but new attribute values are not in the list.", async () => {
+  it("returns error message when the email (partition key) is not in the list.", async () => {
     // ARRANGE
     const account: any = {
-      email: "test3@email.com",
-      forSale: true,
+      email: "unlisted@email.com",
+      password: "unlisted",
+      userName: "TheUnlistedOne",
+      firstName: "",
+      lastName: "",
+      phone: "",
     };
     // ACT
     const result: string = await updateAccount(account);
     // ASSERT
     expect(result).toBe(
-      "Your account wasn't able to update because the changes made are invalid."
+      "Your account wasn't able to update because the email is not associated with any account."
     );
   });
   it("returns false when the email (partition key) is an object", async () => {
