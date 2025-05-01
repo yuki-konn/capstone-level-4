d1419bcbf057198fd210a794af4ab570ec300362
yuki-konn <gakusei.saru@gmail.com>
2025-04-30 22:38:46 -0500

    updated function return type for backend updateAccount function

--- backend/src/modules/updateAccount.ts
+++ backend/src/modules/updateAccount.ts
@@ -2,9 +2,10 @@ import { UpdateCommandInput } from "@aws-sdk/lib-dynamodb";
 import { Account } from "./Account";
 import { dynamoDBClient } from "./dynamoDBclient";
 
-export async function updateAccount(account: Account): Promise<any> {
+export async function updateAccount(account: Account): Promise<string> {
   const isNotAccount = !account;
-  if (isNotAccount) return false;
+  if (isNotAccount)
+    return "Your account wasn't able to update because your account is undefined.";
 
   const { email, password, userName, firstName, lastName, phone } = account;
 
@@ -21,15 +22,19 @@ export async function updateAccount(account: Account): Promise<any> {
   };
 
   const isEmailNotInList = !email;
-  if (isEmailNotInList) return false;
+  if (isEmailNotInList)
+    return "Your account wasn't able to update because your email is undefined.";
 
   const isAttributeNotInList =
     !password || !userName || !firstName || !lastName || !phone;
-  if (isAttributeNotInList) return false;
+  if (isAttributeNotInList)
+    return "Your account wasn't able to update because the changes made are invalid.";
 
   const isEmailObject = typeof email === "object";
-  if (isEmailObject) return false;
+  if (isEmailObject)
+    return "Your account wasn't able to update because your email is invalid.";
 
   const response = await dynamoDBClient().update(request);
-  return response;
+  const isSuccessful = response.$metadata.httpStatusCode === 200;
+  if (isSuccessful) return "Your account has been updated.";
 }
