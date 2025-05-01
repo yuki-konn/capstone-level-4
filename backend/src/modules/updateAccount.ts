8872f426c5c69cc6038690163d703822e75759b6
yuki-konn <gakusei.saru@gmail.com>
2025-05-01 05:26:14 -0500

    updated function for updateAccount according to updated test specifications

--- backend/src/modules/updateAccount.ts
+++ backend/src/modules/updateAccount.ts
@@ -1,13 +1,37 @@
-import { UpdateCommandInput } from "@aws-sdk/lib-dynamodb";
+import { GetCommandInput, UpdateCommandInput } from "@aws-sdk/lib-dynamodb";
 import { Account } from "./Account";
 import { dynamoDBClient } from "./dynamoDBclient";
 
-export async function updateAccount(account: Account): Promise<string> {
-  const isNotAccount = !account;
+export async function updateAccount(targetAccount: Account): Promise<string> {
+  const isNotAccount = !targetAccount;
   if (isNotAccount)
     return "Your account wasn't able to update because your account is undefined.";
 
-  const { email, password, userName, firstName, lastName, phone } = account;
+  const isEmailNotInList = !targetAccount.email;
+  if (isEmailNotInList)
+    return "Your account wasn't able to update because your email is undefined.";
+
+  const isEmailObject =
+    typeof targetAccount.email === "object" ||
+    JSON.stringify(targetAccount.email) === "{}";
+  if (isEmailObject)
+    return "Your account wasn't able to update because your email is invalid.";
+
+  const readEmail = targetAccount.email;
+  // Read Account
+  const readRequest: GetCommandInput = {
+    TableName: "capstone_logins",
+    Key: { email: readEmail },
+  };
+
+  const readResponse = await dynamoDBClient().get(readRequest);
+  let readAccount = readResponse.Item;
+
+  const isNotInList = readAccount === undefined;
+  if (isNotInList)
+    return "Your account wasn't able to update because the email is not associated with any account.";
+
+  const { email, password, userName, firstName, lastName, phone } = readAccount;
 
   const request: UpdateCommandInput = {
     TableName: "capstone_logins",
@@ -20,19 +44,6 @@ export async function updateAccount(account: Account): Promise<string> {
       phone: { Value: phone },
     },
   };
-
-  const isEmailNotInList = !email;
-  if (isEmailNotInList)
-    return "Your account wasn't able to update because your email is undefined.";
-
-  const isAttributeNotInList =
-    !password || !userName || !firstName || !lastName || !phone;
-  if (isAttributeNotInList)
-    return "Your account wasn't able to update because the changes made are invalid.";
-
-  const isEmailObject = typeof email === "object";
-  if (isEmailObject)
-    return "Your account wasn't able to update because your email is invalid.";
 
   const response = await dynamoDBClient().update(request);
   const isSuccessful = response.$metadata.httpStatusCode === 200;
