911581ee87f2dfce3d5e3e8275e16ccc468687d3
yuki-konn <gakusei.saru@gmail.com>
2025-05-01 07:38:48 -0500

    created deleteAccount function based on test specifications

---
+++ frontend/src/modules/account/deleteAccount.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { Account } from "./Account";
+
+export async function deleteAccount(targetAccount: Account): Promise<string> {
+  const isNotAccount = !targetAccount;
+  if (isNotAccount)
+    return "Your account was unable to be deleted because the account is undefined.";
+
+  const { email, password } = targetAccount;
+
+  const isEmailEmpty = !email;
+  if (isEmailEmpty)
+    return "Your account was unable to be deleted because the email is empty or undefined.";
+
+  const isEmailObject =
+    typeof email === "object" || JSON.stringify(email) === "{}";
+  if (isEmailObject)
+    return "Your account was unable to be deleted because the email is an object.";
+
+  const query = `?email=${email}&password=${password}`;
+  const rootpath = "http://localhost:8000";
+  const route = "/remove";
+  const url = rootpath + route + query;
+  const response = await axios.get(url);
+  const result = response.data;
+  return result;
+}
