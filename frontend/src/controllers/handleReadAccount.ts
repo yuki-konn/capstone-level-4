3cb974d2fa8e9f86d34814fd146d877fe04043ea
yuki-konn <gakusei.saru@gmail.com>
2025-04-30 22:05:30 -0500

    created readAccount handler for form submission

---
+++ frontend/src/controllers/handleReadAccount.ts
@@ -0,0 +1,21 @@
+import { Account } from "../modules/account/Account";
+import { readAccount } from "../modules/account/readAccount";
+
+export async function handleReadAccount(event: any): Promise<Account | string> {
+  event.preventDefault();
+
+  const targetEmail = event.target[0].value;
+  const targetPassword = event.target[1].value;
+
+  const newAccount: Account = {
+    email: targetEmail,
+    password: targetPassword,
+    userName: "",
+    firstName: "",
+    lastName: "",
+    phone: "",
+  };
+
+  const response = await readAccount(newAccount);
+  return response;
+}
