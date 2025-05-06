1e791fe1613e46c136cd0fb8c9a1988f35f3dc81
yuki-konn <gakusei.saru@gmail.com>
2025-05-06 07:04:20 -0500

    updated ShopCard class

--- frontend/src/modules/Shop/ShopCard.tsx
+++ frontend/src/modules/Shop/ShopCard.tsx
@@ -1,21 +1,22 @@
 import React from "react";
+import { teaCards } from "./teaCards";
 
 export class ShopCard {
-  shopContent = (<></>);
+  cardContent: any = (<></>);
 
-  constructor(cardProperty = <></>) {
-    this.shopContent = (
+  constructor(position: number) {
+    this.cardContent = (
       <article className="card border border-success border-2">
         <img
           className="card-img-top"
-          id={cardProperty.id}
-          src={cardProperty.src}
-          data-bs-title={cardProperty.tooltipTitle}
+          id={teaCards[position].id}
+          src={teaCards[position].src}
+          data-bs-title={teaCards[position].tooltipTitle}
           data-bs-toggle="tooltip"
         />
         <div className="card-body">
-          <h3 className="card-title">{cardProperty.title}</h3>
-          {cardProperty.description}
+          <h3 className="card-title">{teaCards[position].title}</h3>
+          {teaCards[position].description}
         </div>
       </article>
     );
