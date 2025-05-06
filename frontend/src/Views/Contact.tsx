2276c3bddab50417fcf92844ae5fb6c634830c4f
yuki-konn <gakusei.saru@gmail.com>
2025-05-06 05:50:35 -0500

    converted Contact page useStates to useSelectors

--- frontend/src/Views/Contact.tsx
+++ frontend/src/Views/Contact.tsx
@@ -3,28 +3,63 @@ import { handleSubmitPhone } from "../controllers/handleSubmitPhone";
 import "./Contact.scss";
 import { getCheckedRadio } from "../modules/Contact/getCheckedRadio";
 import { getServerFeedback } from "../modules/Contact/getServerFeedback";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectContactCommentArea,
+  selectContactDidMount,
+  selectContactFormData1,
+  selectContactFormEmail,
+  selectContactFormName,
+  selectContactRadioArea,
+  selectContactResponse,
+} from "../modules/redux/stateSelectors";
+import { set } from "../modules/redux/store";
+import { ContactFormContent } from "./ContactFormContent";
+import { ContactPhoneContent } from "./ContactPhoneContent";
 // import { handleSubmitContact } from "../controllers/handleSubmitContact";
 // import { databaseSimulation } from "../modules/Contact/databaseSimulation";
 
 export function Contact() {
-  const [didMount, setDidMount] = useState(false);
+  const didMount = useSelector(selectContactDidMount);
+  // SELECTORS FOR CONTACT FORM
+  let contactFormData: any = useSelector(selectContactFormData1);
+  const contactFormName = useSelector(selectContactFormName);
+  const contactFormEmail = useSelector(selectContactFormEmail);
+  let contactFormComment: any = useSelector(selectContactCommentArea);
+  let contactFormRadio: any = useSelector(selectContactRadioArea);
+  let contactFormResponse: any = useSelector(selectContactResponse);
 
-  // OUTPUT FOR CONTACT FORM
-  const [formData1, setFormData1] = useState(<></>);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-
-  const [commentArea, setCommentArea] = useState(<></>);
-  const [radioButtonArea, setRadioButtonArea] = useState(<></>);
-  // FOR PROCESSING FORM
-  const [serverResponse, setServerResponse] = useState(
-    <b style={{ color: "gold" }}>Processing contact form...</b>
-  );
+  const dispatch = useDispatch();
 
   // LIFECYCLE COMPONENTS
   useEffect(componentDidMount, []); // MOUNT HOOK
-  useEffect(componentDidUpdate, [didMount, name]); // UPDATE HOOK
+  useEffect(componentDidUpdate, [didMount, contactFormName]); // UPDATE HOOK
   useEffect(componentDidUnmount, []); // UNMOUNT HOOK
+
+  if (contactFormData === "contactFormData") {
+    contactFormData = (
+      <>
+        <b style={{ color: "green" }}>{contactFormResponse}</b>
+        <br />
+        <div>
+          <span>
+            <u>Form Info</u>
+          </span>
+          <br />
+          <span>Name: {contactFormName}</span>
+          <br />
+          <span>Email: {contactFormEmail}</span>
+          <br />
+          <span>Comment: {contactFormComment}</span>
+          <br />
+          <span>Rating: {contactFormRadio}</span>
+          <br />
+          --------------
+          <br />
+        </div>
+      </>
+    );
+  }
 
   return (
     <main id="contactMain" className="container-lg">
@@ -34,94 +69,14 @@ export function Contact() {
       <section>
         <h2 className="textShadow">Contact Form</h2>
         <form id="form1" onSubmit={handleSubmitContact}>
-          <div className="row">
-            <label className="col-2" htmlFor="contact-name" title="Required">
-              Name *
-            </label>
-            <input
-              required
-              id="contact-name"
-              className="col-10"
-              type="text"
-              name="contact-name"
-              placeholder="John Smith"
-            />
-          </div>
-          <div className="row">
-            <label className="col-2" htmlFor="contact-email" title="Required">
-              Email *
-            </label>
-            <input
-              required
-              id="contact-email"
-              className="col-10"
-              type="email"
-              name="contact-email"
-              placeholder="email@email.com"
-            />
-          </div>
-          <div className="row">
-            <label htmlFor="contact-comment">Comment (Optional)</label>
-            <textarea id="contact-comment" className="w100"></textarea>
-          </div>
-          <fieldset>
-            <legend>Tea Cup Rating (Optional)</legend>
-            <div id="ratingDiv">
-              <label htmlFor="rating-five">
-                5 <i className="bi bi-cup-hot-fill"></i>
-              </label>
-              <input
-                id="rating-five"
-                name="ratings"
-                type="radio"
-                aria-label="5 cups of tea!"
-              />
-              <label htmlFor="rating-four">
-                4 <i className="bi bi-cup-hot-fill"></i>
-              </label>
-              <input
-                id="rating-four"
-                name="ratings"
-                type="radio"
-                aria-label="4 cups of tea!"
-              />
-              <label htmlFor="rating-three">
-                3 <i className="bi bi-cup-hot-fill"></i>
-              </label>
-              <input
-                id="rating-three"
-                name="ratings"
-                type="radio"
-                aria-label="3 cups of tea!"
-              />
-              <label htmlFor="rating-two">
-                2 <i className="bi bi-cup-hot-fill"></i>
-              </label>
-              <input
-                id="rating-two"
-                name="ratings"
-                type="radio"
-                aria-label="2 cups of tea!"
-              />
-              <label htmlFor="rating-one">
-                1 <i className="bi bi-cup-hot-fill"></i>
-              </label>
-              <input
-                id="rating-one"
-                name="ratings"
-                type="radio"
-                aria-label="1 cup of tea!"
-              />
-            </div>
-          </fieldset>
-          <input className="w100" type="submit" />
+          <ContactFormContent />
         </form>
         <br />
         <div
           id="outputTag1"
           className="border border-3 border-dark text-center"
         >
-          {formData1}
+          {contactFormData}
         </div>
 
         <br />
@@ -132,45 +87,7 @@ export function Contact() {
         </h2>
 
         <form onSubmit={handleSubmitPhone as any}>
-          <div className="row">
-            <label className="col-2" htmlFor="phone-name" title="Required">
-              Name *
-            </label>
-            <input
-              required
-              id="phone-name"
-              className="col-10"
-              type="text"
-              name="phone-name"
-              placeholder="John Smith"
-            />
-          </div>
-          <div className="row">
-            <label className="col-2" htmlFor="phone-number" title="Required">
-              Phone Number *
-            </label>
-            <input
-              required
-              id="phone-number"
-              className="col-10"
-              type="tel"
-              name="phone-number"
-              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-              placeholder="###-###-####"
-            />
-          </div>
-          <div className="row">
-            <label className="col-2" htmlFor="phone-date-time" title="Required">
-              Appointment Time *
-            </label>
-            <input
-              required
-              id="phone-date-time"
-              className="col-10"
-              type="datetime-local"
-            />
-          </div>
-          <input className="w100" type="submit" />
+          <ContactPhoneContent />
         </form>
         <br />
         <div
@@ -187,8 +104,9 @@ export function Contact() {
   // MOUNT PHASE
   function componentDidMount() {
     document.title = "Yuki Tea Shop | CONTACT";
-    setDidMount(true);
     console.log("The Contact component has mounted.");
+    let action = set.contactDidMount(true);
+    dispatch(action);
   }
 
   // UPDATE PHASE
@@ -196,29 +114,9 @@ export function Contact() {
     if (didMount) {
       console.log("The Contact component has updated.");
       // RUNS AFTER handleSubmitContact IS CLICKED.
-      if (name) {
-        setFormData1(
-          <>
-            {serverResponse}
-            <br />
-            <div>
-              <span>
-                <u>Form Info</u>
-              </span>
-              <br />
-              <span>Name: {name}</span>
-              <br />
-              <span>Email: {email}</span>
-              <br />
-              <span>Comment: {commentArea}</span>
-              <br />
-              <span>Rating: {radioButtonArea}</span>
-              <br />
-              --------------
-              <br />
-            </div>
-          </>
-        );
+      if (contactFormName) {
+        let action = set.contactFormData1("contactFormData");
+        dispatch(action);
       }
     }
   }
@@ -234,33 +132,36 @@ export function Contact() {
     // MAKES SUBMISSION NOT DISPLAY MORE THAN ONCE
     const isEmpty = outputTag.children.length === 0;
     if (isEmpty) {
-      setName(name);
-      setEmail(email);
+      let action = set.contactFormName(name);
+      dispatch(action);
+      action = set.contactFormEmail(email);
+      dispatch(action);
 
       // ADDS INPUTTED COMMENT
-      setCommentArea(comment);
+      action = set.contactCommentArea(comment);
+      dispatch(action);
 
       // CHECKS TO SEE WHICH RADIO BUTTON IS SELECTED
       const ratingFeedback = getCheckedRadio(event);
       // ADDS INPUTTED RATING
-      setRadioButtonArea(ratingFeedback);
-
-      setFormData1(<>{serverResponse}</>);
+      action = set.contactRadioArea(ratingFeedback);
+      dispatch(action);
 
       // SERVER DATA RETRIEVAL
       const serverFeedback = getServerFeedback(event);
-      // CHANGES FROM PROCESSING TO SUCCESSFULLY SUBMITTED
-      setServerResponse(<b style={{ color: "green" }}>{serverFeedback}</b>);
 
-      // const serverFeedback = databaseSimulation(event);
-      // setServerResponse(<b style={{ color: "green" }}>{serverFeedback}</b>);
+      // CHANGES FROM PROCESSING TO SUCCESSFULLY SUBMITTED
+      action = set.contactResponse(serverFeedback);
+      dispatch(action);
     }
   }
-}
 
-// UNMOUNT PHASE
-function componentDidUnmount() {
-  return function () {
-    console.log("The Contact component has unmounted.");
-  };
+  // UNMOUNT PHASE
+  function componentDidUnmount() {
+    return function () {
+      console.log("The Contact component has unmounted.");
+      let action = set.contactDidMount(false);
+      dispatch(action);
+    };
+  }
 }
