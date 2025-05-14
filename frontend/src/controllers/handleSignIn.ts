e058f09029645bbb42080aecf87f80853cc34bb3
yuki-konn <gakusei.saru@gmail.com>
2025-05-14 12:16:08 -0500

    converted get method in handleSignIn to post method

--- frontend/src/controllers/handleSignIn.ts
+++ frontend/src/controllers/handleSignIn.ts
@@ -1,35 +1,27 @@
 import axios from "axios";
+import { getRootPathAws } from "../utils/getRootPathAws";
 // import { authenticationAws } from "../modules/Contact/authenticationAws";
 
 // FOR BACKEND AWS DYNAMODB
-// WHEN async IS CALLED IT RETURNS A NEW PROMISE, WHICH WILL BE RESOLVED BY THE VALUE RETURNED BY THE ASYNC FUNCTION.
 export async function handleSignIn(event: any, onSignIn: any) {
   event.preventDefault();
 
   const form = event.target;
   const email = event.target[1].value; // value of email input
   const password = event.target[2].value; // value of password input
-
   const closeButton = event.target[3];
 
-  // Determines if local or GitHub Pages
-  const domain = window.location.hostname;
-  let rootpath: string = "http://localhost:8000";
-  if (domain === "yuki-konn.github.io")
-    rootpath =
-      "https://qyxgfxhby4ejikmfmdtzmkjnrq0yazfh.lambda-url.us-east-2.on.aws";
+  const rootpath = getRootPathAws();
+  const route = "/authUser";
+  const url = rootpath + route;
+  const data = { email: email, password: password };
 
-  // SENDS QUERY WITH EMAIL AND PASSWORD TO /authUser ROUTE IN BACKEND
-  const path: string = `${rootpath}/authUser?email=${email}&password=${password}`;
-  const response = await axios.get(path);
+  const response = await axios.post(url, data);
   const isAuthenticated = response.data;
 
   if (isAuthenticated) {
-    // Closes Modal
-    closeButton.click();
-
-    // RESET FORM
-    form.reset();
+    closeButton.click(); // Closes Modal
+    form.reset(); // RESET FORM
 
     // Changes button to SignOutModal.
     onSignIn(); // Prop from SignInModal
