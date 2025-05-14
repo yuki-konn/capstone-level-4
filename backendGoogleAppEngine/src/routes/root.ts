d0e7908c81f49b07572107ae4164f0a4eb6fde7c
yuki-konn <gakusei.saru@gmail.com>
2025-05-14 04:56:01 -0500

    updated root handler to return an object

--- backendGoogleAppEngine/src/routes/root.ts
+++ backendGoogleAppEngine/src/routes/root.ts
@@ -5,6 +5,11 @@ dotenv.config();
 
 export function root(request: Request, response: Response) {
   const { url } = request;
-  const context = `<div><h1>Google App Engine Demo</h1><p>Response from Google App Engine.</p><span>Your path is ${url}.</span><br/><h5>mode = ${process.env.mode}</h5></div>`;
-  response.send(context);
+  const data = {
+    header: "Google App Engine Demo",
+    context: "Response from Google App Engine Server.",
+    url: url,
+    mode: process.env.mode,
+  };
+  response.send(data);
 }
