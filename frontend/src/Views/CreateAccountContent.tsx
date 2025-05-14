9a7c020564edce7fdeae3291223669a1c1d29709
yuki-konn <gakusei.saru@gmail.com>
2025-05-14 11:33:46 -0500

    center aligned modal content

--- frontend/src/Views/CreateAccountContent.tsx
+++ frontend/src/Views/CreateAccountContent.tsx
@@ -5,28 +5,40 @@ export function CreateAccountContent() {
     <>
       <span>Required information is marked with *</span>
       <br />
-      <div>
-        <label style={{ width: "25%" }}>Email * </label>
+      <div className="text-center">
+        <label className="text-start" style={{ width: "25%" }}>
+          Email *{" "}
+        </label>
         <input type="email" placeholder="johnsmith@email.com" required />
       </div>
-      <div>
-        <label style={{ width: "25%" }}>Password * </label>
+      <div className="text-center">
+        <label className="text-start" style={{ width: "25%" }}>
+          Password *{" "}
+        </label>
         <input type="password" placeholder="password" required />
       </div>
-      <div>
-        <label style={{ width: "25%" }}>User Name * </label>
+      <div className="text-center">
+        <label className="text-start" style={{ width: "25%" }}>
+          User Name *{" "}
+        </label>
         <input type="text" placeholder="Username" required />
       </div>
-      <div>
-        <label style={{ width: "25%" }}>First Name </label>
+      <div className="text-center">
+        <label className="text-start" style={{ width: "25%" }}>
+          First Name{" "}
+        </label>
         <input type="text" placeholder="John" />
       </div>
-      <div>
-        <label style={{ width: "25%" }}>Last Name </label>
+      <div className="text-center">
+        <label className="text-start" style={{ width: "25%" }}>
+          Last Name{" "}
+        </label>
         <input type="text" placeholder="Smith" />
       </div>
-      <div>
-        <label style={{ width: "25%" }}>Phone Number </label>
+      <div className="text-center">
+        <label className="text-start" style={{ width: "25%" }}>
+          Phone Number{" "}
+        </label>
         <input
           type="tel"
           pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
