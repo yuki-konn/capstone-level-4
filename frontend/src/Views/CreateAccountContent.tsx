a6ea02947d69ca6cbf52208ce8031469893fccdd
yuki-konn <gakusei.saru@gmail.com>
2025-05-19 15:03:24 -0500

    added name attributes to inputs

--- frontend/src/Views/CreateAccountContent.tsx
+++ frontend/src/Views/CreateAccountContent.tsx
@@ -9,31 +9,41 @@ export function CreateAccountContent() {
         <label className="text-start" style={{ width: "25%" }}>
           Email *{" "}
         </label>
-        <input type="email" placeholder="johnsmith@email.com" required />
+        <input
+          type="email"
+          name="email"
+          placeholder="johnsmith@email.com"
+          required
+        />
       </div>
       <div className="text-center">
         <label className="text-start" style={{ width: "25%" }}>
           Password *{" "}
         </label>
-        <input type="password" placeholder="password" required />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          required
+        />
       </div>
       <div className="text-center">
         <label className="text-start" style={{ width: "25%" }}>
           User Name *{" "}
         </label>
-        <input type="text" placeholder="Username" required />
+        <input type="text" name="userName" placeholder="Username" required />
       </div>
       <div className="text-center">
         <label className="text-start" style={{ width: "25%" }}>
           First Name{" "}
         </label>
-        <input type="text" placeholder="John" />
+        <input type="text" name="firstName" placeholder="John" />
       </div>
       <div className="text-center">
         <label className="text-start" style={{ width: "25%" }}>
           Last Name{" "}
         </label>
-        <input type="text" placeholder="Smith" />
+        <input type="text" name="lastName" placeholder="Smith" />
       </div>
       <div className="text-center">
         <label className="text-start" style={{ width: "25%" }}>
@@ -43,6 +53,7 @@ export function CreateAccountContent() {
           type="tel"
           pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
           placeholder="##########"
+          name="phoneNumber"
         />
       </div>
     </>
