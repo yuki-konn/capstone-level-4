cfef46aeb9d7de667ff380fcb0d751c996d4b1f9
yuki-konn <gakusei.saru@gmail.com>
2025-05-21 15:10:28 -0500

    added buttons to shopCard Class

--- frontend/src/modules/Shop/ShopCard.tsx
+++ frontend/src/modules/Shop/ShopCard.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { teaCards } from "./teaCards";
+// import { handleClickIncrease } from "../../controllers/handleClickIncrease";
 
 export class ShopCard {
   cardContent: any = (<></>);
+  position: number;
 
+  //TODO: Get the onClick to work
   constructor(position: number) {
     this.cardContent = (
-      <article className="card border border-success border-2">
+      <article className="card border border-success border-2 m-1">
         <img
           className="card-img-top"
           id={teaCards[position].id}
@@ -14,11 +17,37 @@ export class ShopCard {
           data-bs-title={teaCards[position].tooltipTitle}
           data-bs-toggle="tooltip"
         />
-        <div className="card-body">
+        <span className="badge bg-info col-8">
+          {teaCards[position].saleType}
+        </span>
+        <div className="card-body p-2">
           <h3 className="card-title">{teaCards[position].title}</h3>
-          {teaCards[position].description}
+          <p className="card-text border border-black rounded">
+            {teaCards[position].description}
+          </p>
+
+          <div>
+            <button
+              // onClick={handleClick1(position) as any}
+              className="btn btn-sm border-black bg-success"
+            >
+              {teaCards[position].addButton}
+            </button>
+            <span className="text-black">
+              <i className="bi bi-cart fs-4"></i>
+            </span>
+            <button
+              // onClick="handleClickDecrease(event)"
+              className="btn btn-sm border-black bg-danger"
+            >
+              {teaCards[position].minusButton}
+            </button>
+          </div>
         </div>
       </article>
     );
   }
 }
+// function handleClick1(position: number): void {
+//   handleClickIncrease(position);
+// }
