c28b6693ba5a42efe8c9d14658e64b40c64cfcff
yuki-konn <gakusei.saru@gmail.com>
2025-05-22 18:09:47 -0500

    updated tests

--- backendGoogleAppEngine/src/modules/getAnswer.test.ts
+++ backendGoogleAppEngine/src/modules/getAnswer.test.ts
@@ -11,18 +11,66 @@ describe("getAnswer", { timeout: 100000 }, () => {
     // ASSERT
     expect(response).toBeDefined();
   });
-  it("responds with the correct answer if it's in the context.", async () => {
+  it.only("responds with the correct answer if it's in the context.", async () => {
     // ARRANGE
-    const question = "Which tea has the most caffeine?";
+    const question1 = "Which tea has the most caffeine?";
+    const question2 = "Which tea has the 2nd most caffeine?";
+    const question3 = "Which teas are caffeine-free?";
+    const context = aiContext;
+    // ACT
+    const response1 = await getAnswer(question1, context);
+    const response2 = await getAnswer(question2, context);
+    const response3 = await getAnswer(question3, context);
+    // ASSERT
+    expect(response1).toBe("Matcha tea");
+    expect(response2).toBe("Pu - erh");
+    expect(response3).toBe("rooibos and herbal");
+  });
+  it("doesn't respond with an answer if it's not in the context.", async () => {
+    // ARRANGE
+    const question = "What is T?";
     const context = aiContext;
     // ACT
     const response = await getAnswer(question, context);
     // ASSERT
-    expect(response).toBe("Matcha tea");
+    expect(response).toBeUndefined();
   });
-  //   it("doesn't response with an answer if it's not in the context.", async () => {});
-  //   it("doesn't respond with an answer if there's no question.", async () => {});
-  //   it("doesn't response with an answer if there's no context.", async () => {});
-  //   it("doesn't response with an answer if there's no question and no context.", async () => {});
-  //   it("doesn't response with an answer if either the question or context are empty strings.", async () => {});
+  it("doesn't respond with an answer if there's no question.", async () => {
+    // ARRANGE
+    const question = "";
+    const context = aiContext;
+    // ACT
+    const response = await getAnswer(question, context);
+    // ASSERT
+    expect(response).toBeUndefined();
+  });
+  it("doesn't respond with an answer if there's no context.", async () => {
+    // ARRANGE
+    const question = "Which tea has the most caffeine?";
+    const context = "";
+    // ACT
+    const response = await getAnswer(question, context);
+    // ASSERT
+    expect(response).toBeUndefined();
+  });
+  it("doesn't respond with an answer if there's no question and no context.", async () => {
+    // ARRANGE
+    const question = "";
+    const context = "";
+    // ACT
+    const response = await getAnswer(question, context);
+    // ASSERT
+    expect(response).toBeUndefined();
+  });
+  it("doesn't respond with an answer if either the question or context are empty strings.", async () => {
+    // ARRANGE
+    const question = "";
+    const context = "";
+    // ACT
+    const response1 = await getAnswer(question, "placeholder context");
+    const response2 = await getAnswer("placeholder question", context);
+    // ASSERT
+    expect(response1).toBeUndefined();
+    expect(response2).toBeUndefined();
+  });
 });
