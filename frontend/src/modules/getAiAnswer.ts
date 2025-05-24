3ffa35bda286421a43defef18946176634941337
yuki-konn <gakusei.saru@gmail.com>
2025-05-24 01:32:25 -0500

    asserted datatypes

--- frontend/src/modules/getAiAnswer.ts
+++ frontend/src/modules/getAiAnswer.ts
@@ -4,13 +4,13 @@ import { getRootPathAppEngine } from "../utils/getRootPathAppEngine";
 export async function getAiAnswer(
   userQuestion: string,
   chatBoxContext: string
-) {
+): Promise<string> {
   const data = { question: userQuestion, context: chatBoxContext };
   const rootpath = getRootPathAppEngine();
   const route = "/ai";
   const url = rootpath + route;
   const response = await axios.post(url, data);
-  const answer = response.data;
+  const answer: string = response.data;
   debugger;
   return answer;
 }
