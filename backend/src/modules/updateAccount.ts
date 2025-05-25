3043533c1b8abf4ea20993cef73d95e52caa6917
yuki-konn <gakusei.saru@gmail.com>
2025-05-25 06:42:16 -0500

    updated backend '/update' route handler and updateAccount function

--- backend/src/modules/updateAccount.ts
+++ backend/src/modules/updateAccount.ts
@@ -2,51 +2,44 @@ import { GetCommandInput, UpdateCommandInput } from "@aws-sdk/lib-dynamodb";
 import { Account } from "../models/Account";
 import { dynamoDBClient } from "./dynamoDBclient";
 
-export async function updateAccount(targetAccount: Account): Promise<string> {
-  const isNotAccount = !targetAccount;
-  if (isNotAccount)
-    return "Your account wasn't able to update because your account is undefined.";
-
-  const isEmailNotInList = !targetAccount.email;
-  if (isEmailNotInList)
-    return "Your account wasn't able to update because your email is undefined.";
-
+export async function updateAccount(
+  accountInfo: Account
+): Promise<Account | undefined> {
+  const isNotAccount = !accountInfo;
+  if (isNotAccount) return (accountInfo = undefined);
+  const isEmailNotInList = !accountInfo.email;
+  if (isEmailNotInList) return (accountInfo = undefined);
   const isEmailObject =
-    typeof targetAccount.email === "object" ||
-    JSON.stringify(targetAccount.email) === "{}";
-  if (isEmailObject)
-    return "Your account wasn't able to update because your email is invalid.";
+    typeof accountInfo.email === "object" ||
+    JSON.stringify(accountInfo.email) === "{}";
+  if (isEmailObject) return (accountInfo = undefined);
 
-  const readEmail = targetAccount.email;
-  // Read Account
   const readRequest: GetCommandInput = {
     TableName: "capstone_logins",
-    Key: { email: readEmail },
+    Key: { email: accountInfo.email },
   };
-
   const readResponse = await dynamoDBClient.get(readRequest);
   let readAccount = readResponse.Item;
 
   const isNotInList = readAccount === undefined;
-  if (isNotInList)
-    return "Your account wasn't able to update because the email is not associated with any account.";
-
-  const { email, password, userName, firstName, lastName, phone } =
-    targetAccount;
+  if (isNotInList) return (accountInfo = undefined);
+  const isWrongPassword = accountInfo.password !== readAccount.password;
+  if (isWrongPassword) return (accountInfo = undefined);
 
   const request: UpdateCommandInput = {
     TableName: "capstone_logins",
-    Key: { email: email },
+    Key: { email: accountInfo.email },
     AttributeUpdates: {
-      password: { Value: password },
-      userName: { Value: userName },
-      firstName: { Value: firstName },
-      lastName: { Value: lastName },
-      phone: { Value: phone },
+      userName: { Value: accountInfo.userName },
+      firstName: { Value: accountInfo.firstName },
+      lastName: { Value: accountInfo.lastName },
+      phone: { Value: accountInfo.phone },
     },
   };
-
   const response = await dynamoDBClient.update(request);
-  const isSuccessful = response.$metadata.httpStatusCode === 200;
-  if (isSuccessful) return "Your account has been updated.";
+
+  const isFailed = response.$metadata.httpStatusCode !== 200;
+  if (isFailed) return (accountInfo = undefined);
+
+  return accountInfo;
 }
--- backend/src/routes/update.ts
+++ backend/src/routes/update.ts
@@ -3,7 +3,7 @@ import { Account } from "../models/Account";
 import { updateAccount } from "../modules/updateAccount";
 
 export async function update(request: Request, response: Response) {
-  const query = request.body;
-  const updatedAccount = await updateAccount(query as Account);
+  const account: Account = request.body;
+  const updatedAccount = await updateAccount(account);
   response.send(updatedAccount);
 }
