509d2d2edfef2a51b8a570e86f32c2673440792a
yuki-konn <gakusei.saru@gmail.com>
2025-05-25 04:34:18 -0500

    updated function

--- frontend/src/modules/account/updateAccount.ts
+++ frontend/src/modules/account/updateAccount.ts
@@ -2,27 +2,38 @@ import axios from "axios";
 import { Account } from "../../models/Account";
 import { getRootPathAws } from "../../utils/getRootPathAws";
 
-export async function updateAccount(targetAccount: Account): Promise<string> {
-  const isNotAccount = !targetAccount;
-  if (isNotAccount)
-    return "Your account wasn't able to update because your account is undefined.";
+export async function updateAccount(
+  newUserName: string,
+  newFirstName: string,
+  newLastName: string,
+  newPhone: string
+): Promise<Account | undefined> {
+  // const isNotAccount = !targetAccount;
+  // if (isNotAccount)
+  //   return "Your account wasn't able to update because your account is undefined.";
 
-  const { email } = targetAccount;
+  // const { email } = targetAccount;
 
-  const isEmailUndefined = !email;
-  if (isEmailUndefined)
-    return "Your account wasn't able to update because your email is undefined.";
+  // const isEmailUndefined = !email;
+  // if (isEmailUndefined)
+  //   return "Your account wasn't able to update because your email is undefined.";
 
-  const isEmailObject =
-    typeof email === "object" || JSON.stringify(email) === "{}";
-  if (isEmailObject)
-    return "Your account wasn't able to update because your email is invalid.";
+  // const isEmailObject =
+  //   typeof email === "object" || JSON.stringify(email) === "{}";
+  // if (isEmailObject)
+  //   return "Your account wasn't able to update because your email is invalid.";
 
+  const data = {
+    userName: newUserName,
+    firstName: newFirstName,
+    lastName: newLastName,
+    phone: newPhone,
+  };
   const rootpath = getRootPathAws();
   const route = "/update";
   const url = rootpath + route;
 
-  const response = await axios.post(url, targetAccount);
-  const result: string = response.data;
+  const response = await axios.post(url, data);
+  const result = response.data;
   return result;
 }
